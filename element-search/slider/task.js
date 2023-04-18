548162bbd7d2e816763bd3eed718a067f5cfb2f6
Slava <nlo_tomsk@mail.ru>
2023-04-19 02:51:09 +0700

    2_3

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,28 +1,22 @@
-const sliders = document.querySelectorAll('.slider__item');
-
+const sliders = Array.prototype.slice.call(document.querySelectorAll('.slider__item'));
 const next = document.querySelector('div.slider__arrow.slider__arrow_next');
 const prev = document.querySelector('div.slider__arrow.slider__arrow_prev');
 
-indexSlide = 0;
-
 next.onclick = function () {
-    sliders[indexSlide].className = 'slider__item';
+    let indexSlideNext = sliders.findIndex(el => el.className === 'slider__item slider__item_active')
+    sliders[indexSlideNext].className =('slider__item');
+    sliders[++indexSlideNext % sliders.length].className = ('slider__item slider__item_active')
     
-    if (indexSlide < sliders.length-1) {
-        indexSlide++;
-    } else {
-        indexSlide = 0;
-    }
-    sliders[indexSlide].className = 'slider__item slider__item_active';
 };
 
 prev.onclick = function () {
-    sliders[indexSlide].className = 'slider__item';
-    console.log(sliders[indexSlide]);
-    if (indexSlide > 0) {
-        indexSlide--;
+    let indexSlidePrev = sliders.findIndex(el => el.className === 'slider__item slider__item_active')
+    sliders[indexSlidePrev].className =('slider__item');
+
+    if (indexSlidePrev > 0) {
+        indexSlidePrev--;
     } else {
-        indexSlide = sliders.length - 1;
+        indexSlidePrev = sliders.length - 1;
     }
-    sliders[indexSlide].className = 'slider__item slider__item_active'; 
+    sliders[indexSlidePrev].className = ('slider__item slider__item_active')
 };
