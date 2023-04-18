12b873e59f2df0277a42772e28e2aa09ebdc17a0
Slava <nlo_tomsk@mail.ru>
2023-04-19 03:16:16 +0700

    3-1

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,31 +1,18 @@
-const modalSuccess = document.getElementById("modal_success");
-const dead = document.getElementById("modal_main");
-dead.className = "modal modal_active";
-document.onclick = function (event) {
-    var target = event.target
-    var array = target.className.split(' ');
-    let totalElem = array.length
-    for (let i = 0; i < totalElem; i++) {
-        if (array[i] === 'modal__close') {
-            for (let j = 0; j < totalElem; j++) {
-                if (array[j] === 'show-success') {
-                    modalSuccess.className = "modal modal_active";
-                    return;
-                }
-            }
-            var array2 = dead.className.split(' ');
+const modalSuccess = document.getElementById('modal_success');
+const dead = document.getElementById('modal_main');
+const showSuccess = document.querySelector('.show-success');
+const closeX = document.querySelectorAll('div.modal__close');
 
-            for (let k = 0; k < array2.length; k++) {
-                if (array2[k] === 'modal_active') {
-                    dead.className = "modal";
-                }
-            }
-            var array3 = modalSuccess.className.split(' ');
-            for (let p = 0; p < array3.length; p++) {
-                if (array3[p] === 'modal_active') {
-                    modalSuccess.className = "modal";
-                }
-            }
-        }
-    }
+dead.classList.add('modal_active');
+
+
+showSuccess.onclick = () => {
+    dead.classList.remove('modal_active');
+    modalSuccess.classList.add('modal_active');
 }
+
+closeX.forEach(element => {
+    element.onclick = () => {
+        element.closest('.modal').classList.remove('modal_active');
+    }
+});
